52783a7ac11933691c9b07c447e05d1a97eb2ee6
Leo Rønsholt Mikkelsen <leomikkelsen9@gmail.com>
2018-10-03 09:17:04 +0200

    Added Physics formulary

---
+++ FormsLommeregner/PhysicsFormulary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsLommeregner
+{
+    public class PhysicsFormulary
+    {
+        public const double pi = 3.14;
+        public const double g = 9.82;
+
+        public double PotentialE(double m, double h)
+        {
+            double e = g * m * h;
+            return e;
+        }
+    }
+}

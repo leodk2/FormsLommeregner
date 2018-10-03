fafc065244541d5b36179aaac4f8a7b0a24c0f34
Leo Rønsholt Mikkelsen <leomikkelsen9@gmail.com>
2018-10-03 10:37:06 +0200

    Made the GUI and some other shit

--- FormsLommeregner/Form1.cs
+++ FormsLommeregner/Form1.cs
@@ -19,8 +19,10 @@ namespace FormsLommeregner
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            PhysicsFormulary physicsFormulary = new PhysicsFormulary();
-            physicsFormulary.PotentialEnergyE()
+
+            
+
+
         }
     }
 }

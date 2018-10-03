f141e33c0031d0c9a392536b8bcea2b08865ecad
Asbjørn Brummer Birkelund <asbj1321@edu.nextkbh.dk>
2018-10-03 09:31:49 +0200

    added PotEnrgy methods

--- FormsLommeregner/PhysicsFormulary.cs
+++ FormsLommeregner/PhysicsFormulary.cs
@@ -8,16 +8,27 @@ namespace FormsLommeregner
 {
     public class PhysicsFormulary
     {
-        public const double g = 9.82;
+        public const double gAcc = 9.82;
         public const double pi = 3.14;
         public const double tau = 2 * pi;
         
 
-
-        public double PotentialE(double m, double h)
+        // Potential Energy
+        public double PotentialEnergyE(double mass, double height)
         {
-            double e = g * m * h;
-            return e;
+            double energy = gAcc * mass * height;
+            return energy;
         }
+        public double PotentialEnergyM(double energy, double height)
+        {
+            double mass = energy / (gAcc * height);
+            return mass;
+        }
+        public double PotentialEnergyH(double energy, double mass)
+        {
+            double height = energy / (gAcc * mass);
+            return height;
+        }
+
     }
 }

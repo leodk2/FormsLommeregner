bd17d018154162e8c04a900c1ff248bc423fdb8c
Asbjørn Brummer Birkelund <asbj1321@edu.nextkbh.dk>
2018-10-22 12:51:05 +0200

    expanded login form

--- FormsLommeregner/Login.cs
+++ FormsLommeregner/Login.cs
@@ -19,7 +19,15 @@ namespace FormsLommeregner
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            InternetAndSecurity IAS = new InternetAndSecurity();
+            if (IAS.SqlReader("Uid", EmailField.Text, "Code1", IAS.sqlConnection))
+            {
+                Console.WriteLine("did this");
+                this.Close();
+                Form1 form = new Form1();
+                Application.Run(form);
+                
+            }
         }
     }
 }
